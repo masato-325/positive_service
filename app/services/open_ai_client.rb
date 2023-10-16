ed5f69402492add7dcacf7b4333f45433a897c96
masato <maaton325@gmail.com>
2023-10-16 10:53:01 +0900

    プロンプト編集

--- app/services/open_ai_client.rb
+++ app/services/open_ai_client.rb
@@ -7,7 +7,7 @@ class OpenAiClient
   end
 
   def chat(input, character_info)
-    additional_prompt = "Role-play based on these settings and turn negative thoughts into positive ones:
+    additional_prompt = "以下の設定でロールプレイをして、ネガティブな思考をポジティブなものに変えましょう。
                         Name: #{character_info[:name]},
                         Personality: #{Character.human_attribute_name("personality.#{character_info[:personality]}")},
                         Speech style: #{Character.human_attribute_name("speak_style.#{character_info[:speak_style]}")},
@@ -15,8 +15,10 @@ class OpenAiClient
                         Pronoun: #{Character.human_attribute_name("given_name.#{character_info[:given_name]}")},
                         Age: #{Character.human_attribute_name("age.#{character_info[:age]}")},
                         Gender: #{Character.human_attribute_name("gender.#{character_info[:gender]}")}.
-                        Please answer in 50 characters or less in Japanese.
-                        Leave an unforgettable quote."
+                        自己紹介をしてください
+                        日本語で60文字以内で答えてください。
+                        あなたが可能であると判断した場合具体的な解決策を提案してください。
+                        相談者が忘れられない名言を残してください。"
 
     
     response = @client.chat(
