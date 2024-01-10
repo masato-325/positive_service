bd7020c5aab4a84bfedccdc722d84775d1d4cf53
masato <maaton325@gmail.com>
2024-01-10 21:20:17 +0900

    エラー修正

--- app/forms/consultation_form.rb
+++ app/forms/consultation_form.rb
@@ -8,8 +8,8 @@ class ConsultationForm
     return false unless valid?
     
     ActiveRecord::Base.transaction do
-      character = Character.create!(name: name, personality: personality, speak_style: speak_style, business: business, given_name: given_name, age: age, gender: gender)
-      @consultation = Consultation.create!(title: title, public_status: public_status, user_id: user_id, character_id: character.id, message: message)
+      character = Character.create(name: name, personality: personality, speak_style: speak_style, business: business, given_name: given_name, age: age, gender: gender)
+      @consultation = Consultation.create(title: title, public_status: public_status, user_id: user_id, character_id: character.id, message: message)
     end
     true
   rescue ActiveRecord::RecordInvalid
