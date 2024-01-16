157dd87a571119509e536cc2dd784c3b0afb2e98
masato <maaton325@gmail.com>
2024-01-17 03:04:31 +0900

    本番環境のエラー修正

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
+// app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
+import SearchController from "./search_controller"
 
 const application = Application.start()
+application.register("search", SearchController)
 
 // Configure Stimulus development experience
 application.debug = false
