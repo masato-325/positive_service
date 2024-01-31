037357c3a0e1f2fb67561f944c3ef91045cc3aed
masato <maaton325@gmail.com>
2024-01-31 22:54:17 +0900

    固定のプロンプト（試作）追加

--- app/javascript/custom/consultations_new.js
+++ app/javascript/custom/consultations_new.js
@@ -12,18 +12,37 @@ document.addEventListener('turbo:load', (event) => {
     const randomizeButton = document.getElementById("randomize-button");
     if (randomizeButton) {
       randomizeButton.addEventListener("click", function() {
-        // 性格の選択
         randomSelect("consultation_form_personality");
-        // 話し方の選択
         randomSelect("consultation_form_speak_type");
-        // 職業の選択
         randomSelect("consultation_form_business");
-        // 一人称の選択
         randomSelect("consultation_form_given_name");
-        // 年齢の選択
         randomSelect("consultation_form_age");
-        // 性別の選択
         randomSelect("consultation_form_gender");
+      });
+    }
+    const sisterButton = document.getElementById("sister-button");
+    if (sisterButton) {
+      sisterButton.addEventListener("click", function() {
+        setSelectValue("consultation_form_name", "お姉ちゃん");
+        setSelectValue("consultation_form_personality", "優しい");
+        setSelectValue("consultation_form_speak_type", "関西弁");
+        setSelectValue("consultation_form_business", "姉");
+        setSelectValue("consultation_form_given_name", "うち");
+        setSelectValue("consultation_form_age", "20代");
+        setSelectValue("consultation_form_gender", "女性");
+      });
+    }
+
+    const brotherButton = document.getElementById("brother-button");
+    if (brotherButton) {
+      brotherButton.addEventListener("click", function() {
+        setSelectValue("consultation_form_name", "お兄ちゃん");
+        setSelectValue("consultation_form_personality", "クレイジー");
+        setSelectValue("consultation_form_speak_type", "高圧的");
+        setSelectValue("consultation_form_business", "兄");
+        setSelectValue("consultation_form_given_name", "俺");
+        setSelectValue("consultation_form_age", "20代");
+        setSelectValue("consultation_form_gender", "男性");
       });
     }
   }
@@ -36,4 +55,11 @@ document.addEventListener('turbo:load', (event) => {
     const randomIndex = Math.floor(Math.random() * options.length);
     select.selectedIndex = randomIndex;
   }
+
+  function setSelectValue(elementId, value) {
+    const select = document.getElementById(elementId);
+    if (!select) return; // 要素が見つからない場合は処理をスキップ
+    select.value = value;
+  }
+  
 });
